d9fa8033437e2a213f380474416a22b1fe5cfdb3
Nic Luciano <nicluciano@new-host.home>
2010-05-22 20:38:23 -0400

    adding edit functionality

--- v2/js/ui.js
+++ v2/js/ui.js
@@ -1,4 +1,30 @@
-$(function() {
+OPENLIKE.Ui = new function() {
+
+	this.updateServicePreferences = function(serviceList, btn) {
+		
+			// toggle button in menu
+		var btn = btn.toggleClass('enabled'),
+			// current clicked service name
+			service = btn.text().toLowerCase(),
+			// find the associated button in the widget (needs to be in sync)
+			widgetBtn = $('a.openlike-' + service, serviceList.data('widget')).toggleClass('enabled'),
+			// vertical name
+			vertical = serviceList.attr('data-vertical');
+		
+		// update the preference for each item in the edit list
+		// (the whole array gets updated)
+		$('a', serviceList).each(function() {
+			var btn = $(this),
+				enabled = btn.hasClass('enabled');
+				console.log(enabled);
+			OPENLIKE.Preferences[enabled? 'hide': 'show'](vertical, btn.text().toLowerCase());
+		});
+		
+	}
+	
+};
+
+$(function() { 
 	
 	var s = {
 		widget: '#openlike-widget',
@@ -31,7 +57,10 @@ $(function() {
 				$('body').append(editPanel);
 			}
 			// take the buttons from widget and put them in the panel
-			$(s.editBody).empty().append(serviceNodes);
+			$(s.editBody).empty()
+				.append(serviceNodes)
+				.attr('data-vertical', widget.attr('data-vertical'))
+				.data('widget', widget);
 			// show panel
 			editPanel.show();
 		});
@@ -42,5 +71,20 @@ $(function() {
 			.live('mouseleave', function() {
 				$(this).fadeOut();
 			});	
+			
+		// buttons in the edit panel that turn them on/off
+		$([s.editBody, 'a'].join(' '))
+			// change button enabled
+			.live('click', function(e) {
+					// toggle current button state
+				var btn = $(this),
+					// the list with the services
+					editList = btn.closest(s.editBody);
+				// this goes through each service in the list
+				// and changes its preference accordingly
+				OPENLIKE.Ui.updateServicePreferences(editList, btn);
+				// don't gooo
+				e.preventDefault();
+			});
 	
 });
